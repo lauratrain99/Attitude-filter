ab0c914a7dc11352f64a6baecada5814df5522ee
Laura Train <laura.train@alumnos.uc3m.es>
2021-01-05 10:13:32 +0100

    Yaw does not diverge. Magnetometer improvement pending.

--- kalman/imu_filter.m
+++ kalman/imu_filter.m
@@ -94,9 +94,7 @@ nav.Pp = zeros(LI, 36);          % A posteriori covariance matrices, Pp
 nav.K  = zeros(LI, 18);          % Kalman gain matrices, K
 nav.S  = zeros(LI, 9);           % Innovation matrices, S
 nav.ob = zeros(LI, 1);           % Number of observable states at each acceleromter data
-%nav.deltay_prop = zeros(LI,3);   % Propagated measurement error
 nav.deltar = zeros(LI,3);        % Error residual
-nav.deltay = zeros(LI,3);        % Estimated measurement error
 nav.wb = zeros(LI, 3);           % Corrected angular velocity
 
 % Prior estimates for initial update
@@ -106,7 +104,7 @@ kf.Pi = diag([imu.ini_align_err, imu.gb_dyn].^2);
 ge = 9.81;
 g_n = [0; 0; ge];
 ab = imu.fb(1,:)';
-kf.deltay = -DCMbn*ab - g_n;
+kf.deltar = -DCMbn*ab - g_n;
 
 
 
@@ -115,7 +113,7 @@ kf.H = [skewm(g_n) O];
 
 % Correction covariance matrix, constant value over time
 kf.R = diag([imu.a_std]).^2;
-kf.R = diag([0.0001, 0.0001, 0.0001]).^2;
+%kf.R = diag([0.0001, 0.0001, 0.0001]).^2;
 
 % Propagate prior estimates to get xp(1) and Pp(1)
 kf = kf_update_acc( kf );
@@ -128,13 +126,13 @@ nav.Pi = reshape(kf.Pi, 1, 36);
 
 nav.deltaxp(1,:) = kf.deltaxp;
 nav.Pp(1,:) = reshape(kf.Pp, 1, 36);
-nav.deltay(1,:) = kf.deltay;
-
+nav.deltar(1,:) = kf.deltar;
+nav.wb(1,:) = imu.wb(1,:)' - gb_dyn;
 
 
 % Prediction covariance matrix, constant value over time
 kf.Q  = diag([imu.g_std, imu.gb_dyn].^2);
-kf.Q  = diag([1, 1, 1, 1, 1, 1].^2);
+%kf.Q  = diag([1, 1, 1, 1, 1, 1].^2);
 
 
 for i = 2:LI
@@ -150,13 +148,13 @@ for i = 2:LI
 
       % KALMAN FILTER      
           % PREDICTION         
-          kf.F = [-skewm(wb_corrected) -I; O O];
+          kf.F = [skewm(wb_corrected) -I; O O];
           %kf.J = [-DCMbn -I O; O O I];
           kf.G = [-I O; O I];
           
           % CORRECTION
           ab = imu.fb(i,:)';
-          kf.deltay = -DCMbn*ab - g_n;
+          kf.deltar = -DCMbn*ab - g_n;
           
           % Execution of the Extended Kalman filter
           kf.deltaxp = zeros(length(kf.deltaxp),1);           % states 1:3 are forced to be zero (error-state approach)
@@ -205,9 +203,7 @@ for i = 2:LI
           nav.K(i,:)            = reshape(kf.K, 1, 18);
           nav.S(i,:)            = reshape(kf.S, 1, 9);
           nav.ob(i,:)           = ob;
-          %nav.deltay_prop(i,:)  = kf.deltay_prop;
           nav.deltar(i,:)       = kf.deltar;
-          nav.deltay(i,:)       = kf.deltay;
           nav.wb(i,:)           = wb_corrected;
 
 end
--- synthetic-data/static_IMU/static_IMU.m
+++ synthetic-data/static_IMU/static_IMU.m
@@ -17,67 +17,16 @@ addpath ../../conversions/
 addpath ../../kalman/
 addpath ../../performance_analysis/
 
-%% CONVERSION CONSTANTS
+%% GENERATE SYNTHETIC DATA
 
-G =  9.80665;       % Gravity constant, m/s^2
-G2MSS = G;          % g to m/s^2
-MSS2G = (1/G);      % m/s^2 to g
+imu1.ini_align_err = deg2rad([0.5, 0.5, 0.5]);
+imu1.g_std = [0.01, 0.01, 0.01];
+imu1.a_std = [0.01, 0.01, 0.01];
+imu1.ini_align = deg2rad([0, 0, 0]);
+imu1.gb_dyn = [0.001, 0.001, 0.001]; 
 
-D2R = (pi/180);     % degrees to radians
-R2D = (180/pi);     % radians to degrees
-
-KT2MS = 0.514444;   % knot to m/s
-MS2KMH = 3.6;       % m/s to km/h
-
-%% REFERENCE DATA
-
-load ref.mat
-
-% ref.mat contains a reference data structure from which inertial
-% sensors wil be simulated. It must contain the following fields:
-
-%         t: Nx1 time vector (seconds).
-%       lat: Nx1 latitude (radians).
-%       lon: Nx1 longitude (radians).
-%         h: Nx1 altitude (m).
-%       vel: Nx3 NED velocities (m/s).
-%      roll: Nx1 roll angles (radians).
-%     pitch: Nx1 pitch angles (radians).
-%       yaw: Nx1 yaw angle vector (radians).
-%   DCMnb_m: Nx9 matrix with nav-to-body direct cosine matrices (DCM).
-%            Each row of DCMnb_m contains the 9 elements of a particular DCMnb
-%            matrix ordered as [a11 a21 a31 a12 a22 a32 a13 a23 a33].
-%      freq: sampling frequency (Hz).
-
-%% ADIS16405: IMU1 error profile
-
-% IMU data structure:
-%         t: Ix1 time vector (seconds).
-%        fb: Ix3 accelerations vector in body frame XYZ (m/s^2).
-%        wb: Ix3 turn rates vector in body frame XYZ (radians/s).
-%       arw: 1x3 angle random walks (rad/s/root-Hz).
-%      arrw: 1x3 angle rate random walks (rad/s^2/root-Hz).
-%       vrw: 1x3 velocity random walks (m/s^2/root-Hz).
-%      vrrw: 1x3 velocity rate random walks (m/s^3/root-Hz).
-%     g_std: 1x3 gyros standard deviations (radians/s).
-%     a_std: 1x3 accrs standard deviations (m/s^2).
-%    gb_sta: 1x3 gyros static biases or turn-on biases (radians/s).
-%    ab_sta: 1x3 accrs static biases or turn-on biases (m/s^2).
-%    gb_dyn: 1x3 gyros dynamic biases or bias instabilities (radians/s).
-%    ab_dyn: 1x3 accrs dynamic biases or bias instabilities (m/s^2).
-%   gb_corr: 1x3 gyros correlation times (seconds).
-%   ab_corr: 1x3 accrs correlation times (seconds).
-%    gb_psd: 1x3 gyros dynamic biases PSD (rad/s/root-Hz).
-%    ab_psd: 1x3 accrs dynamic biases PSD (m/s^2/root-Hz);
-%      freq: 1x1 sampling frequency (Hz).
-% ini_align: 1x3 initial attitude at t(1), [roll pitch yaw] (rad).
-% ini_align_err: 1x3 initial attitude errors at t(1), [roll pitch yaw] (rad).
-
-
-load imu1.mat
 N = 10000;
 imu1.t = linspace(0,10,N);
-imu1.ini_align = [0,0,0];
 wbx = zeros(N,1) + imu1.g_std(1)*randn(N,1);
 wby = zeros(N,1) + imu1.g_std(2)*randn(N,1); 
 wbz = zeros(N,1) + imu1.g_std(3)*randn(N,1); 
@@ -120,134 +69,62 @@ saveas(figure(2),'IMU1_angularvelocities_raw.jpg')
 % title('IMU1 Local magnetic field [Gauss]')
 % legend('mx','my','mz')
 % saveas(figure(3),'IMU1_magnetometer_raw.jpg')
-%% ADIS16488 IMU2 error profile
-
-% IMU data structure:
-%         t: Ix1 time vector (seconds).
-%        fb: Ix3 accelerations vector in body frame XYZ (m/s^2).
-%        wb: Ix3 turn rates vector in body frame XYZ (radians/s).
-%       arw: 1x3 angle random walks (rad/s/root-Hz).
-%      arrw: 1x3 angle rate random walks (rad/s^2/root-Hz).
-%       vrw: 1x3 velocity random walks (m/s^2/root-Hz).
-%      vrrw: 1x3 velocity rate random walks (m/s^3/root-Hz).
-%     g_std: 1x3 gyros standard deviations (radians/s).
-%     a_std: 1x3 accrs standard deviations (m/s^2).
-%    gb_sta: 1x3 gyros static biases or turn-on biases (radians/s).
-%    ab_sta: 1x3 accrs static biases or turn-on biases (m/s^2).
-%    gb_dyn: 1x3 gyros dynamic biases or bias instabilities (radians/s).
-%    ab_dyn: 1x3 accrs dynamic biases or bias instabilities (m/s^2).
-%   gb_corr: 1x3 gyros correlation times (seconds).
-%   ab_corr: 1x3 accrs correlation times (seconds).
-%    gb_psd: 1x3 gyros dynamic biases PSD (rad/s/root-Hz).
-%    ab_psd: 1x3 accrs dynamic biases PSD (m/s^2/root-Hz);
-%      freq: 1x1 sampling frequency (Hz).
-% ini_align: 1x3 initial attitude at t(1), [roll pitch yaw] (rad).
-% ini_align_err: 1x3 initial attitude errors at t(1), [roll pitch yaw] (rad).
-
-load imu2.mat
-N = 10000;
-imu2.t = linspace(0,10,N);
-imu2.ini_align = [0,0,0];
-
-wbx = zeros(N,1) + imu2.g_std(1)*randn(N,1);
-wby = zeros(N,1) + imu2.g_std(2)*randn(N,1);
-wbz = zeros(N,1) + imu2.g_std(3)*randn(N,1);
-
-
-imu2.wb = [wbx, wby, wbz];
-
-abx = zeros(N,1) + imu2.a_std(1)*randn(N,1);
-aby = zeros(N,1) + imu2.a_std(2)*randn(N,1);
-abz = -9.81*ones(N,1) + imu2.a_std(3)*randn(N,1);
-
-imu2.fb = [abx, aby, abz];
-
-% mnx = 0.22*ones(length(imu2.t),1);
-% mny = zeros(length(imu2.t),1);
-% mnz = 0.17*ones(length(imu2.t),1);
-% 
-% imu2.mn = [mnx, mny, mnz];
-
-figure(4)
-plot(imu2.t,imu2.fb(:,1), 'r', imu2.t, imu2.fb(:,2), 'b', imu2.t, imu2.fb(:,3), 'g')
-xlabel('Time [s]')
-grid on
-title('IMU2 Acceleration raw measurements body frame [m/s^2]')
-legend('ax','ay','az')
-saveas(figure(4),'IMU1_accelerations_raw.jpg')
-
-figure(5)
-plot(imu2.t,imu2.wb(:,1), 'r', imu2.t, imu2.wb(:,2), 'b', imu2.t, imu2.wb(:,3), 'g')
-xlabel('Time [s]')
-grid on
-title('IMU2 Angular velocity measurements body frame [rad/s]')
-legend('wx','wy','wz')
-saveas(figure(5),'IMU1_angularvelocities_raw.jpg')
 
 %% SENSOR FUSION AND FILTER
 
 [nav1, kf1] = imu_filter(imu1);
 
-[nav2, kf2] = imu_filter(imu2);
 
 
+%% COMPARISON ATTITUDE COMPUTER
+[quat, euler] = attitude_computer(imu1);
 
-%% PLOTS
 
-figure(6)
-plot(nav1.t, nav1.qua(:,1), 'r', nav1.t, nav1.qua(:,2), 'c', nav1.t, nav1.qua(:,3), 'g', nav1.t, nav1.qua(:,4), 'k')
+figure(5)
+plot(imu1.t, quat(:,1), 'r', imu1.t, quat(:,2), 'c', imu1.t, quat(:,3), 'g', imu1.t, quat(:,4), 'k')
 xlabel('Time [s]')
 legend('q1', 'q2', 'q3', 'q4')
 grid minor
-title('ADIS16405 (IMU1) quaternions')
-%xlim([-20, imu1.t(end) + 20])
-ylim([-1.2 1.2])
+title('ADIS16405 (IMU1) attitude computer quaternions')
 legend('location','southeast')
-saveas(figure(6),'IMU1_quaternions_EKF.jpg')
+
+
+
+figure(6)
+plot(imu1.t, rad2deg(euler(:,1)), 'r', imu1.t, rad2deg(euler(:,2)), 'b', imu1.t, rad2deg(euler(:,3)), 'g')
+legend('roll [deg]', 'pitch [deg]', 'yaw [deg]')
+xlabel('Time [s]')
+grid minor
+title('ADIS16405 (IMU1) attitude computer Euler angles')
+
+for i = 1:length(nav1.t)
+    nav1.quat_error_norm(i,1) = norm(nav1.deltaxp(1:3,:));
+    nav1.dyn_bias_norm(i,1) = norm(nav1.deltaxp(4:6,:));
+end
 
 
 figure(7)
-plot(nav1.t, nav1.roll, 'r', nav1.t, nav1.pitch, 'b', nav1.t, nav1.yaw, 'g')
-legend('roll [deg]', 'pitch [deg]', 'yaw [deg]')
+plot(nav1.t, nav1.quat_error_norm, 'r', nav1.t, nav1.dyn_bias_norm, 'b')
 xlabel('Time [s]')
 grid minor
-title('ADIS16405 (IMU1) Euler angles')
-%xlim([-20, imu1.t(end) + 20])
-saveas(figure(7),'IMU1_EulerAngles_EKF.jpg')
+legend('\deltaq','\delta\zeta')
+%xlim([-20, nav1.t(end) + 20])
+title('Errors')
+
 
 figure(8)
-plot(nav2.t, nav2.roll, 'r', nav2.t, nav2.pitch, 'b', nav2.t, nav2.yaw, 'g')
-legend('roll [deg]', 'pitch [deg]', 'yaw [deg]')
+plot(imu1.t, quat(:,1), 'r', imu1.t, quat(:,2), 'c', imu1.t, quat(:,3), 'g', imu1.t, quat(:,4), 'k', ...
+     nav1.t, nav1.qua(:,1), '--r', nav1.t, nav1.qua(:,2), '--c', nav1.t, nav1.qua(:,3), '--g', nav1.t, nav1.qua(:,4), '--k')
 xlabel('Time [s]')
+legend('q1', 'q2', 'q3', 'q4','q1 Kalman','q2 Kalman','q3 Kalman', 'q4 Kalman')
 grid minor
-title('ADIS16488 (IMU2) Euler angles')
-%xlim([-20, imu1.t(end) + 20])
-saveas(figure(8),'IMU2_EulerAngles_EKF.jpg')
-
+title('Attitude computer vs Kalman filter. Quaternions')
+legend('location','southeast')
 
 figure(9)
-plot(nav2.t, nav2.qua(:,1), 'r', nav2.t, nav2.qua(:,2), 'c', nav2.t, nav2.qua(:,3), 'g', nav2.t, nav2.qua(:,4), 'k')
-xlabel('Time [s]')
-legend('q1', 'q2', 'q3', 'q4')
-grid minor
-title('ADIS16488 (IMU2) quaternions')
-%xlim([-20, imu1.t(end) + 20])
-ylim([-1.2 1.2])
-legend('location','southeast')
-saveas(figure(9),'IMU2_quaternions_EKF.jpg')
-
-for i = 1:length(nav1.t)
-    nav1.quat_error_norm(i,1) = norm(nav1.deltaxp(i,:));
-end
-
-for i = 1:length(nav2.t)
-    nav2.quat_error_norm(i,1) = norm(nav2.deltaxp(i,:));
-end
-
-figure(10)
-plot(nav1.t, nav1.quat_error_norm, 'r', nav2.t, nav2.quat_error_norm, 'b')
-legend('\deltaq IMU1', '\deltaq IMU2')
+plot(imu1.t, rad2deg(euler(:,1)), 'r', imu1.t, rad2deg(euler(:,2)), 'b', imu1.t, rad2deg(euler(:,3)), 'g', ...
+     nav1.t, nav1.roll, '--r', nav1.t, nav1.pitch, '--b', nav1.t, nav1.yaw, '--g')
+legend('roll', 'pitch', 'yaw', 'roll Kalman', 'pitch Kalman', 'yaw Kalman')
 xlabel('Time [s]')
 grid minor
-%xlim([-20, nav1.t(end) + 20])
-title('Quaternion error')
+title('Attitude computer vs Kalman filter. Euler angles [deg]')
